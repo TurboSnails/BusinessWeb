a7d75a59e1f1e4db7800d3a5f4a7ed30a9e1dde1
hassan <hassan@u1s1.biz>
2025-12-24 01:09:26 +0800

    Update vite.config.js with base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true }
+  // 将 '你的仓库名' 替换为 BusinessWeb
+  base: '/BusinessWeb/', 
+  server: { 
+    host: true 
+  }
 })
