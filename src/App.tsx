092e7ae845c7410609ab511e6136a3983d9c9c0e
hassan <hassan@u1s1.biz>
2025-12-24 01:33:44 +0800

    Change base path from BusinessWeb to a

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Pulse from './pages/Pulse'
 export default function App(): JSX.Element {
   return (
     <div className="app">
-      <BrowserRouter basename="/BusinessWeb">
+      <BrowserRouter basename="/a">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
